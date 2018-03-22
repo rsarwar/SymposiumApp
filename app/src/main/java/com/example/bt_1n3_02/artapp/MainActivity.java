e70cdf0d96e71bb7716cb4e0a5db5e44737d7be3
BT_1N3_02 <rsarwar4537@bths.edu>
2018-03-22 13:07:50 -0400

    worked on code for gallery

--- app/src/main/java/com/example/bt_1n3_02/artapp/MainActivity.java
+++ app/src/main/java/com/example/bt_1n3_02/artapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.bt_1n3_02.artapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Integer[] images{R.drawable.flower1, R.drawable.flower2, R.drawable.tiger};
+    private ImageView imgview;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -39,6 +49,47 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Gallery imgGallery = (Gallery) findViewById(R.id.gallery);
+
+        imgGallery.setAdapter(new ImageAdapter(this));
+        imgview= (ImageView) findViewById(R.id.img);
+        imgGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int arg2, long id) {
+                Toast.makeText(getApplicationContext(),"Image"+arg2, Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 
+    public class ImageAdapter extends BaseAdapter{
+        private Context context;
+        int ImageBackground;
+        public ImageAdapter(Context context){
+            this.context=context;
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int arg0, View convertView, ViewGroup parent) {
+            ImageView imageView= new ImageView(context);
+            imageView.setImageResource(images[arg0]);
+            return null;
+        }
+    }
 }
