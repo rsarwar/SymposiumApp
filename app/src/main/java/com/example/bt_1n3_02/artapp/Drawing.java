a54294f0812edae0274b9d78067cab01b60d46da
Ramisha <rsarwar4537@bths.edu>
2018-06-09 17:21:47 -0400

    login fixed and drawing class added

---
+++ app/src/main/java/com/example/bt_1n3_02/artapp/Drawing.java
@@ -0,0 +1,28 @@
+package com.example.bt_1n3_02.artapp;
+
+import android.graphics.Path;
+
+/**
+ * Created by BT_1N3_03 on 6/8/2018.
+ */
+
+public class Drawing {
+
+    public int color;
+    public boolean enboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public Drawing(int color, boolean enboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.enboss = enboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+}
+/*
+The following packages are not available:
+- Package id platforms;android-P
+ */
