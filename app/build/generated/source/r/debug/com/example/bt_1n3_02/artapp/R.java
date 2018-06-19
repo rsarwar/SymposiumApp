f9f0b29749d827acc9e144997888da9485a35aec
BT_1N3_04 <rsarwar4537@bths.edu>
2018-06-19 16:11:48 -0400

    changed icon and few details

--- app/build/generated/source/r/debug/com/example/bt_1n3_02/artapp/R.java
+++ app/build/generated/source/r/debug/com/example/bt_1n3_02/artapp/R.java
@@ -3802,6 +3802,7 @@ public final class R {
   public static final class mipmap {
     public static final int ic_launcher=0x7f0c0000;
     public static final int ic_launcher_round=0x7f0c0001;
+    public static final int paintbrsh=0x7f0c0002;
   }
   public static final class string {
     public static final int abc_action_bar_home_description=0x7f0d0000;
